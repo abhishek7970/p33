808b7e5e30481af1cb97ed2ca1000735cad0b2d2
abhishek7970 <ayadavabhishek12345@gmail.com>
2026-02-09 14:03:43 +0530

    done

--- project_3/src/main/java/in/co/rays/project_3/util/HibDataSource.java
+++ project_3/src/main/java/in/co/rays/project_3/util/HibDataSource.java
@@ -1,27 +1,33 @@
 package in.co.rays.project_3.util;
 
-import org.hibernate.HibernateException;
+import java.util.ResourceBundle;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import in.co.rays.project_3.exception.ApplicationException;
 
 /**
  * Hibernate DataSource is provides the object of session factory and session
  * 
  * 
- * @author Abhishek Yadav
+ * @author Abhishek yadav
  *
  */
 public class HibDataSource {
 	private static SessionFactory sessionFactory = null;
 
 	public static SessionFactory getSessionFactory() {
-
 		if (sessionFactory == null) {
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-		}
+            ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
+
+            String jdbcUrl = System.getenv("DATABASE_URL");
+            if (jdbcUrl == null || jdbcUrl.trim().isEmpty()) {
+                jdbcUrl = rb.getString("url");
+            }
+           
+            
+            sessionFactory = new Configuration().configure()
+            		.setProperty("hibernate.connection.url", jdbcUrl).buildSessionFactory();		}
 		return sessionFactory;
 	}
 
@@ -31,19 +37,13 @@ public class HibDataSource {
 		return session;
 
 	}
+	
+	
 
 	public static void closeSession(Session session) {
 
 		if (session != null) {
 			session.close();
 		}
-		
 	}
-	public static void handleException(HibernateException e) throws ApplicationException {
-
-		// DB down / connection issue
-		throw new ApplicationException("Database Server is down. Please try after some time");
-	}
-
 }
-
